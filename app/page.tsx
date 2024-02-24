e4d3cd6ea61548934b35ed4715b8ca942dd11e61
NatiFekadu <Natifekadu808@gmail.com>
2024-02-24 21:42:15 +0300

    implement main layout

--- app/page.tsx
+++ app/page.tsx
@@ -7,8 +7,31 @@ export default function Home() {
   const { arrayToSort, isSorting } = useSortingAlgorithmContext();
 
   return (
-    <div>
-      <h1>Hello World</h1>
-    </div>
+    <main className="absolute top-0 h-screen w-screen z-[2] bg-[#000000] bg-[radial-gradient(#ffffff33_1px,#150229_1px)] bg-[size:40px_40px]">
+      <div className="flex h-full justify-center">
+        <div
+          id="content-container"
+          className="flex max-w-[1020px] w-full flex-col lg:px-0 px-4"
+        >
+          <div className="h-[66px] relative flex items-center justify-between w-full">
+            <h1 className="text-2xl font-light text-center text-gray-300 hidden md:flex">
+              Sorting Visualizer
+            </h1>
+            <div>Controls</div>
+          </div>
+          <div className="relative h-[calc(1000vh-66px)] w-full">
+            <div className="absolute bottom-[32px] w-full mx-auto left-0 right-0 flex justify-center items-end">
+              {arrayToSort.map((value, index) => (
+                <div
+                  key={index}
+                  className="array-line relative w-1 mx-0.5 shadow-lg opacity-70 rounded-lg default-line-color"
+                  style={{height:`${value}px`}}
+                ></div>
+              ))}
+            </div>
+          </div>
+        </div>
+      </div>
+    </main>
   );
 }
